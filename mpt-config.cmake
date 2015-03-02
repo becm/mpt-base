1cdd574bba7ac3a3e25430adfc9ffeef142e20fe
Marc Becker <marc.h.becker@gmx.de>
2015-03-02 02:17:08 +0100

    cmake build improvements
    use `mpt-config.cmake` for path configuration
    build client library
    fix: includes and library linking

--- mpt-config.cmake
+++ mpt-config.cmake
@@ -6,17 +6,16 @@ set(CMAKE_INSTALL_RPATH "$ORIGIN/")
 # target path options
 set(MPT_INSTALL_LIB lib/${CMAKE_LIBRARY_ARCHITECTURE}
     CACHE FILEPATH "runtime libraries path (install prefix prepended if relative)")
+if (NOT IS_ABSOLUTE ${MPT_INSTALL_LIB})
+  set(MPT_INSTALL_LIB ${CMAKE_INSTALL_PREFIX}/${MPT_INSTALL_LIB})
+endif()
 
 set(MPT_INSTALL_INCLUDE include
     CACHE FILEPATH "header files (install prefix prepended if relative)")
-
 if (NOT IS_ABSOLUTE ${MPT_INSTALL_INCLUDE})
   set(MPT_INSTALL_INCLUDE ${CMAKE_INSTALL_PREFIX}/${MPT_INSTALL_INCLUDE})
 endif()
 
-if (NOT IS_ABSOLUTE ${MPT_INSTALL_LIB})
-  set(MPT_INSTALL_LIB ${CMAKE_INSTALL_PREFIX}/${MPT_INSTALL_LIB})
-endif()
 
 # release information
 set(MPT_RELEASE "" CACHE STRING "set to non-emty for release info")
