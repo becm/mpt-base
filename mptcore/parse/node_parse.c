1d2eefd4cebf1bc01f0cdb3302fd13a3873b24c7
Marc Becker <marc.h.becker@gmx.de>
2016-02-02 16:54:41 +0100

    fix: parsing argument assignment

--- mptcore/parse/node_parse.c
+++ mptcore/parse/node_parse.c
@@ -30,11 +30,14 @@ extern int mpt_node_parse(MPT_STRUCT(node) *conf, const MPT_STRUCT(value) *val, 
 	if (!val) {
 		return MPT_ERROR(BadArgument);
 	}
+	format = 0;
+	limit = "ns";
+	
 	if (!val->fmt) {
 		if (!(fname = val->ptr)) {
 			return MPT_ERROR(BadArgument);
 		}
-		len = 1;
+		len = 0;
 	}
 	else if (val->fmt[0] == MPT_ENUM(TypeFile)) {
 		FILE * const *ptr = val->ptr;
@@ -50,31 +53,29 @@ extern int mpt_node_parse(MPT_STRUCT(node) *conf, const MPT_STRUCT(value) *val, 
 			return MPT_ERROR(BadValue);
 		}
 		len = 1;
+		
+		if (val->fmt[1]) {
+			if (val->fmt[1] != 's') {
+				if (log) mpt_log(log, __func__, MPT_FCNLOG(Error), "%s", MPT_tr("bad format description"));
+				return MPT_ERROR(BadType);
+			}
+			format = ptr[1];
+			len = 2;
+			
+			if (val->fmt[2]) {
+				if (val->fmt[2] != 's') {
+					if (log) mpt_log(log, __func__, MPT_FCNLOG(Error), "%s", MPT_tr("bad name limit"));
+					return MPT_ERROR(BadType);
+				}
+				limit = ptr[2];
+				len = 3;
+			}
+		}
 	}
 	else {
 		return MPT_ERROR(BadType);
 	}
-	format = 0;
-	limit = "ns";
 	
-	if (val->fmt[1]) {
-		char * const *ptr = val->ptr;
-		if (val->fmt[1] != 's') {
-			if (log) mpt_log(log, __func__, MPT_FCNLOG(Error), "%s", MPT_tr("bad format description"));
-			return MPT_ERROR(BadType);
-		}
-		format = ptr[1];
-		len = 2;
-		
-		if (val->fmt[2]) {
-			if (val->fmt[2] != 's') {
-				if (log) mpt_log(log, __func__, MPT_FCNLOG(Error), "%s", MPT_tr("bad name limit"));
-				return MPT_ERROR(BadType);
-			}
-			limit = ptr[2];
-			len = 3;
-		}
-	}
 	if (fname && !(fd = fopen(fname, "r"))) {
 		if (log) mpt_log(log, __func__, MPT_FCNLOG(Error), "%s: %s", MPT_tr("failed to open file"), fname);
 		return MPT_ERROR(BadValue);
