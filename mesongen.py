809a2cf91a5cb62cd107f136111848756415e973
Marc Becker <marc.h.becker@gmx.de>
2016-04-26 18:30:26 +0200

    improve `meson` file generator
    mesongen.py
      fix: support for python 2.x
      use regular expression to identify file marker
      fix: handle header without newline

--- mesongen.py
+++ mesongen.py
@@ -1,48 +1,29 @@
 #!/usr/bin/env python3
 # mesongen.py: update special meson.build files
-import sys, os, fnmatch, io
+import sys, os, fnmatch, io, re
 
+# marker to process file
 marker = b'meson.build.gen'
 buildfile = 'meson.build'
+newline = os.linesep.encode('utf-8')
+matchexp = re.compile(b'#\s*' + marker + b'\s*')
 
 def isgenerated(hdr):
-  elem = hdr.split()
-  
-  if len(elem) == 0:
-    return False
-  
-  if elem[0] == b'#' + marker + b':':
-    return True
-  
-  if len(elem) < 2 or elem[0] != b'#':
-    return False
-  
-  if elem[1] == marker + b':':
-    return True
-  
-  if len(elem) < 3 or elem[1] != marker or elem[2] != b':':
-    return False
-  
-  return True
-
-def newline(f):
-  f.write(bytes(os.linesep, 'utf-8'))
-
-def write_header(f, t, n):
-  f.write(b' # ')
-  f.write(bytes(t, 'utf-8'))
-  f.write(b': ')
-  f.write(bytes(n, 'utf-8'))
-  newline(f)
+  ''' get end of header marker '''
+  match = matchexp.match(hdr)
+  return match if match == None else match.end()
 
 def write_entries(f, elem):
+  ''' write file entries to output '''
   for e in elem:
     f.write(b'  \'')
-    f.write(bytes(e, 'utf-8'))
-    f.write(b'\',')
-    newline(f)
+    f.write(e.encode('utf-8'))
+    f.write(b'\',' + newline)
 
 def content(pattern, path='.', match=None):
+  ''' collect matching filenames '''
+  if type(pattern) == str:
+    pattern = pattern.split()
   buf = []
   for f in os.listdir(path):
     fn = os.path.join(path, f)
@@ -67,30 +48,32 @@ def content(pattern, path='.', match=None):
   return buf
 
 def update(path=buildfile, clear=False):
+  ''' rewrite build file '''
   header = open(path, 'rb').readline()
-  
-  if not isgenerated(header):
+  pos = isgenerated(header)
+  if pos == None:
     return False
   
   if clear == True:
     open(path, 'wb').write(header)
     return True
   
-  tmp = str(header, 'utf-8').split(':')[1].strip()
-  buf = content(tmp.split(), os.path.dirname(path))
+  pattern = header[pos:].decode('utf-8')
+  buf = content(pattern.split(), os.path.dirname(path))
   buf.sort()
   
   with open(path, 'wb') as out:
     out.write(header)
-    out.write(b'src = files(')
-    newline(out)
+    if not pattern.endswith(os.linesep):
+      out.write(newline)
+    out.write(b'src = files(' + newline)
     write_entries(out, buf)
-    out.write(b')')
-    newline(out)
+    out.write(b')' + newline)
   
   return True
 
 def update_recursive(path='.', clear=False):
+  ''' rewrite all build files in path'''
   for root, dirs, files in os.walk(path):
     if files.count(buildfile):
       update(os.path.join(root, buildfile), clear)
@@ -108,7 +91,7 @@ if __name__ == "__main__":
           exit(1)
   elif sys.argv[1] == 'gen':
     if len(sys.argv) < 3:
-      update_recursive('.', clear=True)
+      update_recursive('.')
     else:
       for d in sys.argv[2:]:
         if not update(d):
