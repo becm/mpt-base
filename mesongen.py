f4164d81df6693ad6bc957d9ccb084838a34700d
Marc Becker <marc.h.becker@gmx.de>
2016-04-26 02:24:55 +0200

    change `meson` build generator pattern matching
    mesongen.py
      change to glob(3) style pattern matching
      filename-only statements also match files in subdirs (like `git`)

--- mesongen.py
+++ mesongen.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
-# meson_gen.py: update special meson.build files
+# mesongen.py: update special meson.build files
 import sys, os, fnmatch, io
 
 marker = b'meson.build.gen'
-
-def config(line):
-  line = line.split()
-  
-  suffix = []
-  files = []
-  dirs = []
-  for i in line:
-    i = i.split('=')
-    if i[0] == 'suffix':
-      suffix = i[1].split(',')
-    if i[0] == 'files':
-      files = i[1].split(',')
-    if i[0] == 'directories' or i[0] == 'dirs':
-      dirs = i[1].split(',')
-  
-  return (dirs, suffix, files)
+buildfile = 'meson.build'
 
 def isgenerated(hdr):
   elem = hdr.split()
@@ -42,7 +26,7 @@ def isgenerated(hdr):
   return True
 
 def newline(f):
-    mb.write(bytes(os.linesep, 'utf-8'))
+  f.write(bytes(os.linesep, 'utf-8'))
 
 def write_header(f, t, n):
   f.write(b' # ')
@@ -58,56 +42,77 @@ def write_entries(f, elem):
     f.write(b'\',')
     newline(f)
 
-for root, dirs, files in os.walk('.'):
-  for name in fnmatch.filter(files, 'meson.build'):
-    name = os.path.join(root,name)
-    mb = open(name, 'rb')
-    header = mb.readline()
-    
-    if not isgenerated(header): continue;
-    
-    if len(sys.argv) < 2 or sys.argv[1] == 'gen':
-      True
-    elif sys.argv[1] == 'clean':
-      open(name, 'wb').write(header)
-      continue
-    
-    
-    #mb = open(name, 'w')
-    mb = io.BytesIO()
-    
-    tmp = str(header, 'utf-8').split(':')[1].strip()
-    
-    mb.write(header)
-    mb.write(b'src = files(')
-    newline(mb)
-    
-    dirs, suffixes, files = config(tmp)
-    
-    for d in dirs:
-      write_header(mb, 'directory', d)
-      d = os.path.join(root,d)
-      elem = os.listdir(d)
-      elem.sort()
-      write_entries(mb, elem)
-    
-    for s in suffixes:
-      s = '*.' + s
-      write_header(mb, 'match', s)
-      elem = []
-      for f in os.listdir(root):
-        if fnmatch.fnmatch(f, s):
-          elem = elem + [f]
-      elem.sort()
-      write_entries(mb, elem)
-    
-    if len(files):
-      mb.write(b' # files')
-      newline(mb)
-      files.sort()
-      write_entries(mb, files)
-    
-    mb.write(b')')
-    newline(mb)
-    
-    open(name, 'wb').write(mb.getvalue())
+def content(pattern, path='.', match=None):
+  buf = []
+  for f in os.listdir(path):
+    fn = os.path.join(path, f)
+    if os.path.isfile(fn):
+      m = './' + f if match == None else match + '/' + f
+      for p in pattern:
+        #print(m,p)
+        if not p.count('/'):
+          if fnmatch.fnmatch(f, p):
+            #print(' +',m)
+            buf.append(f if match == None else m)
+            break
+        else:
+          if fnmatch.fnmatch(m, p):
+            #print(' +',m)
+            buf.append(f if match == None else m)
+            break
+        #print(' -',m)
+    elif os.path.isdir(fn):
+      m = f if match == None else match + '/' + f
+      buf = buf + content(pattern, fn, m)
+  return buf
+
+def update(path=buildfile, clear=False):
+  header = open(path, 'rb').readline()
+  
+  if not isgenerated(header):
+    return False
+  
+  if clear == True:
+    open(path, 'wb').write(header)
+    return True
+  
+  tmp = str(header, 'utf-8').split(':')[1].strip()
+  buf = content(tmp.split(), os.path.dirname(path))
+  buf.sort()
+  
+  with open(path, 'wb') as out:
+    out.write(header)
+    out.write(b'src = files(')
+    newline(out)
+    write_entries(out, buf)
+    out.write(b')')
+    newline(out)
+  
+  return True
+
+def update_recursive(path='.', clear=False):
+  for root, dirs, files in os.walk(path):
+    if files.count(buildfile):
+      update(os.path.join(root, buildfile), clear)
+
+if __name__ == "__main__":
+  from sys import exit, stderr
+  if len(sys.argv) < 2:
+    update_recursive('.')
+  elif sys.argv[1] == 'clean':
+    if len(sys.argv) < 3:
+        update_recursive('.', clear=True)
+    else:
+      for d in sys.argv[2:]:
+        if not update(d, clear=True):
+          exit(1)
+  elif sys.argv[1] == 'gen':
+    if len(sys.argv) < 3:
+      update_recursive('.', clear=True)
+    else:
+      for d in sys.argv[2:]:
+        if not update(d):
+          exit(1)
+  else:
+    stderr.write('bad operation: ' + sys.argv[1] + os.linesep)
+    exit(2)
