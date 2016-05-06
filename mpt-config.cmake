5bab567d3b8e5afb1873428d20e9c90b02e45ab0
Marc Becker <marc.h.becker@gmx.de>
2016-05-06 23:25:35 +0200

    improve release definition for `cmake`

--- mpt-config.cmake
+++ mpt-config.cmake
@@ -3,6 +3,12 @@
 # create path-independant libraries
 set(CMAKE_INSTALL_RPATH "$ORIGIN/")
 
+# release information
+set(MPT_RELEASE "" CACHE STRING "set to non-emty for release info")
+if (MPT_RELEASE)
+  add_definitions(-DRELEASE_BUILD="${MPT_RELEASE}")
+endif(MPT_RELEASE)
+
 # target path options
 set(MPT_INSTALL_LIB lib/${CMAKE_LIBRARY_ARCHITECTURE}
     CACHE FILEPATH "runtime libraries path (install prefix prepended if relative)")
