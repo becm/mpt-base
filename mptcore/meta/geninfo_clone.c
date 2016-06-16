1d901e4f510f3a80215624b490bebe9ec39ae3e4
Marc Becker <marc.h.becker@gmx.de>
2016-06-16 21:24:21 +0200

    split clone operation from metatype creator
    mptcore
      move `_mpt_geninfo_clone()` to separate file to avoid C++ conflict

---
+++ mptcore/meta/geninfo_clone.c
@@ -0,0 +1,43 @@
+/*!
+ * default metatype
+ */
+
+#include <stdlib.h>
+
+#include <sys/uio.h>
+
+#include "core.h"
+
+/*!
+ * \ingroup mptMeta
+ * \brief generic metatype clone
+ * 
+ * Create clone of geninfo data in new metatype instance.
+ * 
+ * \param info  start of (meta)data for geninfo
+ * 
+ * \return new metatype instance
+ */
+extern MPT_INTERFACE(metatype) *_mpt_geninfo_clone(const uint64_t *info)
+{
+	static const char vecfmt[] = { 'c' - 0x40, 0 };
+	MPT_INTERFACE(metatype) *meta;
+	MPT_STRUCT(value) val;
+	struct iovec vec;
+	int len;
+	
+	len = _mpt_geninfo_value((uint64_t *) info, 0);
+	
+	if (!(meta = mpt_meta_new(len + 1))) {
+		return 0;
+	}
+	vec.iov_len = len;
+	vec.iov_base = (void *) (info + 1);
+	val.fmt = vecfmt;
+	val.ptr = &vec;
+	if (meta->_vptr->assign(meta, &val) >= 0) {
+		return meta;
+	}
+	meta->_vptr->unref(meta);
+	return 0;
+}
--- mptcore/meta/meta_new.c
+++ mptcore/meta/meta_new.c
@@ -55,39 +55,6 @@ static const MPT_INTERFACE_VPTR(metatype) _vptr_control = {
 
 /*!
  * \ingroup mptMeta
- * \brief generic metatype clone
- * 
- * Create clone of geninfo data in new metatype instance.
- * 
- * \param info  start of (meta)data for geninfo
- * 
- * \return new metatype instance
- */
-extern MPT_INTERFACE(metatype) *_mpt_geninfo_clone(const uint64_t *info)
-{
-	static const char vecfmt[] = { 'c' - 0x40, 0 };
-	MPT_INTERFACE(metatype) *meta;
-	MPT_STRUCT(value) val;
-	struct iovec vec;
-	int len;
-	
-	len = _mpt_geninfo_value((uint64_t *) info, 0);
-	
-	if (!(meta = mpt_meta_new(len + 1))) {
-		return 0;
-	}
-	vec.iov_len = len;
-	vec.iov_base = (void *) (info + 1);
-	val.fmt = vecfmt;
-	val.ptr = &vec;
-	if (meta->_vptr->assign(meta, &val) >= 0) {
-		return meta;
-	}
-	meta->_vptr->unref(meta);
-	return 0;
-}
-/*!
- * \ingroup mptMeta
  * \brief new metatype
  * 
  * Create default metatype
