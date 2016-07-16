d32bf044108aabf1cded1a09f6f5a8f34ad55abd
Marc Becker <marc.h.becker@gmx.de>
2016-07-16 14:11:24 +0200

    update `meson` build files
    examples
      fix: use new `println()` in C++ layout example

--- examples/cxx/layout.cpp
+++ examples/cxx/layout.cpp
@@ -22,9 +22,9 @@ extern int main(int argc, char *argv[])
 	
 	long l;
 	unsigned long u;
-	mpt::print("long = '%c', '%c'", mpt::typeIdentifier(l), mpt::typeIdentifier(u));
+	mpt::println("long = '%c', '%c'", mpt::typeIdentifier(l), mpt::typeIdentifier(u));
 	long double ld;
-	mpt::print("long double = '%c'", mpt::typeIdentifier(ld));
+	mpt::println("long double = '%c'", mpt::typeIdentifier(ld));
 	
 	if (argc < 2) {
 		mpt::warning(__func__, "%s", "missing layout argument");
