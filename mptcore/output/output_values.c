f2f1e1fc10d8119f12dc3d6d10ca0c9a2f6e0a12
Marc Becker <marc.h.becker@gmx.de>
2017-02-20 03:18:45 +0100

    fix: value push checks and erroneous termination
    mptcore
      fix: do not terminate message in value push operation
      smaller output buffer for value alignment
      force complete push of value data to output

--- mptcore/output/output_values.c
+++ mptcore/output/output_values.c
@@ -22,35 +22,53 @@
  */
 extern int mpt_output_values(MPT_INTERFACE(output) *out, int len, const double *val, int ld)
 {
-	int ret;
+	ssize_t ret;
 	
 	if (len < 0) {
 		return MPT_ERROR(BadArgument);
 	}
 	/* special advance condition */
 	if (ld != 1) {
-		uint8_t buf[1024];
+		uint8_t buf[256];
+		ssize_t take;
+		int curr, parts = 0;
 		do {
-			int curr = sizeof(buf)/sizeof(*val);
-			if (curr > len) curr = len;
+			take = sizeof(buf);
+			curr = take / sizeof(*val);
+			if (curr > len) {
+				curr = len;
+				take = curr * sizeof(*val);
+			}
 			/* make (partial) aligned data */
 			mpt_copy64(curr, val, ld, buf, 1);
-			if ((ret = out->_vptr->push(out, curr*sizeof(*val), buf)) < 0) {
-				break;
+			/* push all aligned data */
+			while (take) {
+				if ((ret = out->_vptr->push(out, take, buf)) < 0) {
+					return ret;
+				}
+				if (ret > take) {
+					return MPT_ERROR(BadValue);
+				}
+				take -= ret;
 			}
 			len -= curr;
 			val += curr * ld;
+			++parts;
 		} while (len);
-		len = 1;
+		return parts;
 	} else {
-		ret = out->_vptr->push(out, len*sizeof(*val), val);
-		len = 0;
+		size_t total = len * sizeof(*val);
+		
+		while (total) {
+			if ((ret = out->_vptr->push(out, total, val)) < 0) {
+				return ret;
+			}
+			if ((size_t) ret > total) {
+				return MPT_ERROR(BadValue);
+			}
+			total -= ret;
+		}
+		return 1;
 	}
-	if (ret < 0 || (ret = out->_vptr->push(out, 0, 0)) < 0) {
-		out->_vptr->push(out, 1, 0);
-		return ret;
-	}
-	
-	return len;
 }
 
