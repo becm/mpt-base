e137da4121b27272f37dc34f22dc75085023884d
Marc Becker <becm@gmx.de>
2017-06-22 20:14:30 +0200

    improve value format assignment from metatype

--- mptcore/convert/valfmt_set.c
+++ mptcore/convert/valfmt_set.c
@@ -22,9 +22,9 @@ extern int mpt_valfmt_set(MPT_STRUCT(array) *arr, const MPT_INTERFACE(metatype) 
 {
 	MPT_INTERFACE(iterator) *it;
 	MPT_STRUCT(array) tmp = MPT_ARRAY_INIT;
+	MPT_STRUCT(value) val = MPT_VALUE_INIT;
 	MPT_STRUCT(valfmt) fmt;
-	char *from;
-	int ret;
+	int ret, curr;
 	
 	if (!src) {
 		MPT_STRUCT(buffer) *buf;
@@ -38,39 +38,60 @@ extern int mpt_valfmt_set(MPT_STRUCT(array) *arr, const MPT_INTERFACE(metatype) 
 		}
 		return buf->_used / sizeof(MPT_STRUCT(valfmt));
 	}
-	if ((ret = src->_vptr->conv(src, MPT_ENUM(TypeValFmt), &fmt)) >= 0
-	    && (ret = src->_vptr->conv(src, MPT_ENUM(TypeIterator), &it)) >= 0
-	    && it) {
-		int curr;
+	/* get elements from iterator */
+	it = 0;
+	if ((ret = src->_vptr->conv(src, MPT_ENUM(TypeIterator), &it)) > 0) {
 		if ((curr = mpt_valfmt_add(&tmp, fmt)) < 0) {
 			return MPT_ERROR(BadOperation);
 		}
 		ret = 0;
-		src = (void *) it;
-		while ((curr = it->_vptr->advance(it)) >= 0
-		       && (curr = src->_vptr->conv(src, MPT_ENUM(TypeValFmt), &fmt)) >= 0) {
+		while ((curr = it->_vptr->get(it, MPT_ENUM(TypeValFmt), &fmt)) > 0) {
+			if (it->_vptr->advance(it) < 0) {
+				break;
+			}
 			if (mpt_valfmt_add(&tmp, fmt) < 0) {
 				mpt_array_clone(&tmp, 0);
 				return MPT_ERROR(BadOperation);
 			}
 			++ret;
 		}
-		mpt_array_clone(arr, &tmp);
-		mpt_array_clone(&tmp, 0);
-		return ret;
 	}
-	if ((ret = src->_vptr->conv(src, 's', &from)) < 0) {
-		return ret;
-	}
-	else if (!from) {
-		mpt_array_clone(arr, 0);
-	}
-	else {
-		int curr;
-		if ((curr = mpt_valfmt_parse(&tmp, from)) >= 0) {
-			mpt_array_clone(arr, &tmp);
+	/* get elements from value */
+	else if ((ret = src->_vptr->conv(src, MPT_ENUM(TypeValue), &val)) > 0 && val.fmt) {
+		static const char valfmt[] = { MPT_ENUM(TypeValFmt), 0 };
+		if (!val.ptr) {
+			return MPT_ERROR(BadValue);
 		}
-		mpt_array_clone(&tmp, 0);
+		ret = 0;
+		while ((curr = mpt_value_read(&val, valfmt, &fmt))) {
+			if (curr < 0) {
+				mpt_array_clone(&tmp, 0);
+				return MPT_ERROR(BadType);
+			}
+			if (mpt_valfmt_add(&tmp, fmt) < 0) {
+				mpt_array_clone(&tmp, 0);
+				return MPT_ERROR(BadOperation);
+			}
+			++ret;
+		}
 	}
-	return 0;
+	/* get elements from string */
+	else {
+		char *from;
+		
+		if ((ret = src->_vptr->conv(src, 's', &from)) < 0) {
+			return ret;
+		}
+		else if (!from) {
+			mpt_array_clone(arr, 0);
+			return 0;
+		}
+		else if ((curr = mpt_valfmt_parse(&tmp, from)) < 0) {
+			return curr;
+		}
+		ret = 0;
+	}
+	mpt_array_clone(arr, &tmp);
+	mpt_array_clone(&tmp, 0);
+	return ret;
 }
