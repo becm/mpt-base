aea5414e5220b8bfcd01cfa68cd92da9e4d787eb
Marc Becker <becm@gmx.de>
2017-09-18 16:44:17 +0200

    fix: remove inline spaces in proxy description

--- mptcore/client/proxy_type.c
+++ mptcore/client/proxy_type.c
@@ -14,10 +14,10 @@
  * 
  * Create metatype proxy to library instance.
  * 
- * \param descr symbol @ library to open
- * \param out   logging instance
+ * \param descr type description for type
+ * \param end   extended description for type
  * 
- * \return metatype proxy pointer
+ * \return type id for description
  */
 extern int mpt_proxy_type(const char *desc, const char **end)
 {
@@ -29,38 +29,38 @@ extern int mpt_proxy_type(const char *desc, const char **end)
 	}
 	/* no separator befor symbol end */
 	if ((sep = strchr(desc, ':'))) {
-		if (!(len = sep - desc)) {
+		if (!(len = sep++ - desc)) {
 			return MPT_ERROR(BadValue);
 		}
-		if (!*++sep) {
-			return MPT_ERROR(BadArgument);
-		}
 	}
-	if (len ? !strncmp(desc, "metatype:", len+1) : !strcmp(desc, "metatype")) {
+	if (len ? !strncmp(desc, "metatype:", len + 1) : !strcmp(desc, "metatype")) {
 		type = MPT_ENUM(TypeMeta);
 	}
-	else if (len ? !strncmp(desc, "logger:", len+1) : (!strcmp(desc, "logger") || !strcmp(desc, "log"))) {
+	else if (len ? !strncmp(desc, "logger:", len + 1) : (!strcmp(desc, "logger") || !strcmp(desc, "log"))) {
 		type = MPT_ENUM(TypeLogger);
 	}
-	else if (len ? !strncmp(desc, "io:", len+1) : (!strcmp(desc, "io") || !strcmp(desc, "I/O"))) {
+	else if (len ? !strncmp(desc, "io:", len + 1) : (!strcmp(desc, "io") || !strcmp(desc, "I/O"))) {
 		type = MPT_ENUM(TypeIODevice);
 	}
-	else if (len ? !strncmp(desc, "input:", len+1) : !strcmp(desc, "input")) {
+	else if (len ? !strncmp(desc, "input:", len + 1) : !strcmp(desc, "input")) {
 		type = MPT_ENUM(TypeInput);
 	}
-	else if (len ? !strncmp(desc, "object:", len+1) : !strcmp(desc, "object")) {
+	else if (len ? !strncmp(desc, "object:", len + 1) : !strcmp(desc, "object")) {
 		type = MPT_ENUM(TypeObject);
 	}
-	else if (len ? !strncmp(desc, "output:", len+1) : (!strcmp(desc, "output") || !strcmp(desc, "out"))) {
+	else if (len ? !strncmp(desc, "output:", len + 1) : (!strcmp(desc, "output") || !strcmp(desc, "out"))) {
 		type = MPT_ENUM(TypeOutput);
 	}
-	else if (len ? !strncmp(desc, "solver:", len+1) : !strcmp(desc, "solver")) {
+	else if (len ? !strncmp(desc, "solver:", len + 1) : !strcmp(desc, "solver")) {
 		type = MPT_ENUM(TypeSolver);
 	}
 	else {
 		return MPT_ERROR(BadValue);
 	}
 	if (end) {
+		while ((len = *sep) && isspace(len)) {
+			++sep;
+		}
 		*end = sep;
 	}
 	return type;
