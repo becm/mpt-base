903f310aeeb28b76ef7ed4eaedb83da7a423ec55
Marc Becker <becm@gmx.de>
2022-08-13 18:36:11 +0200

    allow generic value in object property assignment
    mptcore
      use verbatim value for matching type request
      resolve converter and traits on value content access
      catch 'uninitialized value' condition
    
    mpt++
      fix 'meson' build setting for point source file

--- mptcore/object/object_set_value.c
+++ mptcore/object/object_set_value.c
@@ -10,31 +10,47 @@
 
 #include "meta.h"
 
-struct valueConverter
-{
-	MPT_INTERFACE(convertable) _ctl;
-	MPT_TYPE(data_converter) conv;
-	const void *data;
-	size_t size;
-	int type;
-};
-
 static int valueConvert(MPT_INTERFACE(convertable) *conv, int type, void *ptr)
 {
-	const struct valueConverter *val = (void *) conv;
+	const MPT_STRUCT(value) *val = *((void **) (conv + 1));
+	MPT_TYPE(data_converter) convert;
+	
 	if (!type) {
 		static const uint8_t fmt[] = { MPT_ENUM(TypeValue), 0 };
 		if (ptr) *((const uint8_t **) ptr) = fmt;
 		return val->type;
 	}
-	if (val->conv) {
-		int ret = val->conv(val->data, type, ptr);
+	/* verbatim value assignment */
+	if (type == MPT_ENUM(TypeValue)) {
+		if (ptr) {
+			memcpy(ptr, val, sizeof(*val));
+		}
+		return type;
+	}
+	/* type must be generic */
+	if (val->_namespace) {
+		return MPT_ERROR(BadType);
+	}
+	/* value must be initialized */
+	if (!val->ptr) {
+		return MPT_ERROR(MissingData);
+	}
+	/* try conversion base->target */
+	if ((convert = mpt_data_converter(val->type))) {
+		int ret = convert(val->ptr, type, ptr);
 		if (ret >= 0) {
 			return ret;
 		}
 	}
-	if (type == val->type && val->size) {
-		if (ptr) memcpy(ptr, val->data, val->size);
+	/* allow verbatim copy of basic content */
+	if (type == val->type) {
+		const MPT_STRUCT(type_traits) *traits = mpt_type_traits(val->type);
+		if(!traits || traits->init || traits->fini || !traits->size) {
+			return MPT_ERROR(BadType);
+		}
+		if (ptr) {
+			memcpy(ptr, val->ptr, traits->size);
+		}
 		return MPT_ENUM(TypeValue);
 	}
 	return MPT_ERROR(BadType);
@@ -45,7 +61,8 @@ static int valueConvert(MPT_INTERFACE(convertable) *conv, int type, void *ptr)
  * \brief set object property
  * 
  * Set property data to value.
- * Use iterator as intermediate for type conversions.
+ * Allow access to verbatim value
+ * or try to convert/copy value to target type.
  * 
  * \param obj  object interface descriptor
  * \param prop name of property to change
@@ -54,21 +71,13 @@ static int valueConvert(MPT_INTERFACE(convertable) *conv, int type, void *ptr)
 extern int mpt_object_set_value(MPT_INTERFACE(object) *obj, const char *prop, const MPT_STRUCT(value) *val)
 {
 	static const MPT_INTERFACE_VPTR(convertable) ctl = { valueConvert };
-	const MPT_STRUCT(type_traits) *traits;
-	struct valueConverter conv = { { &ctl }, 0, 0, 0, 0 };
+	struct {
+		MPT_INTERFACE(convertable) _ctl;
+		const MPT_STRUCT(value) *val;
+	} conv = { { &ctl }, 0 };
 	
-	if (!val) {
+	if (!(conv.val = val)) {
 		return obj->_vptr->set_property(obj, prop, 0);
 	}
-	conv.conv = mpt_data_converter(val->type);
-	traits = mpt_type_traits(val->type);
-	if (traits && !traits->init && !traits->fini && traits->size) {
-		conv.size = traits->size;
-	}
-	else if (!conv.conv) {
-		return MPT_ERROR(BadType);
-	}
-	conv.type = val->type;
-	conv.data = val->ptr;
 	return obj->_vptr->set_property(obj, prop, &conv._ctl);
 }
