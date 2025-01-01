0d7eb1b04eac743be001f4c193858de66128c415
Marc Becker <becm@gmx.de>
2025-01-01 17:07:05 +0100

    fix(examples): remove old code behavior tests
    remove memory layout and SFINAE code in C++ mpt::object example

--- examples/cxx/object.cpp
+++ examples/cxx/object.cpp
@@ -1,5 +1,5 @@
 /*!
- * test MPT object interface
+ * test MPT object operations
  */
 
 #include <typeinfo>
@@ -9,14 +9,7 @@
 # define MPT_INCLUDE(x) <mpt/x>
 #endif
 
-#include MPT_INCLUDE(node.h)
-#include MPT_INCLUDE(config.h)
-#include MPT_INCLUDE(message.h)
-#include MPT_INCLUDE(convert.h)
-
 #include MPT_INCLUDE(layout.h)
-
-#include MPT_INCLUDE(object.h)
 
 #ifdef __GLIBC__
 # include <mcheck.h>
@@ -24,83 +17,19 @@
 # define mtrace()
 #endif
 
-#define toString(x) #x
-
-class Double {
-	public:
-	Double(double d) { _d = d; }
-	virtual ~Double() { }
-	virtual int f0() { return _d; }
-	virtual int f1(int) { return 1; }
-	virtual int f2(int) { return 2; }
-	virtual int f3(int) { return 3; }
-	virtual int f4(int) { return 4; }
-	virtual int f5(int) { return 5; }
-	virtual int f6(int) { return 6; }
-	virtual int f7(int) { return 7; }
-	private:
-	double _d;
-};
-
-
-class Int {
-	public:
-	Int(int d) { _d = d; }
-	virtual ~Int() { }
-	static int (*get)();
-	virtual int f0() { return _d; }
-	private:
-	int _d;
-};
-
-// SFINAE test
-template <typename T>
-class has_get
-{
-public:
-	operator bool () const
-	{ return test<T>(0); }
-private:
-	template <typename C> static bool test(__decltype(&C::get))
-	{ return true; }
-	template <typename C> static bool test(...)
-	{ return false; }
-};
-
-static int _print_type(void *, mpt::convertable *c, const mpt::collection *)
-{
-	if (c) {
-		std::cout << "next type: " << typeid(*c).name() << std::endl;
-		return 0;
-	}
-	return mpt::BadValue;
-}
-
 extern int main(int , char * const [])
 {
 	mtrace();
 	
-	std::cout << "int: " << has_get<Int>() << std::endl;
-	std::cout << "dbl: " << has_get<Double>() << std::endl;
-	std::cout << toString(hallo) << std::endl;
+	// use graphic items
+	mpt::layout::line li;
+	mpt::layout::graph::axis ax;
 	
-	Double d(46);
-	std::cout << d.f0() << std::endl;
+	std::cout << "ao: " << ax.type() << std::endl;
+	std::cout << "lo: " << li.type() << std::endl;
 	
-	mpt::config::root config;
-	config.query(0, _print_type, 0);
-	mpt::path p("next");
-	config.set("next", "val");
-	config.query(&p, _print_type, 0);
-	mpt::layout::line *li = new mpt::reference<mpt::layout::line>::type;
-	
-	
-	mpt::layout::graph::axis *ax = new mpt::reference<mpt::layout::graph::axis>::type;
-	
-	std::cout << "ao: " << ax->type() << std::endl;
-	std::cout << "lo: " << li->type() << std::endl;
-	
-	for (const auto &i : *li) {
+	// display properties of line
+	for (const auto &i : li) {
 		const mpt::property &p = i;
 		mpt::color col;
 		if (p.val.get(col)) {
@@ -110,26 +39,28 @@ extern int main(int , char * const [])
 		}
 	}
 	
+	// validate object instance dispatch
 	mpt::object *obj;
-	obj = ax;
+	obj = &ax;
 	std::cout << "type(ao): " << typeid(*obj).name() << std::endl;
-	obj = li;
+	obj = &li;
 	std::cout << "type(lo): " << typeid(*obj).name() << std::endl;
 	
-	mpt::object &lr = *li;
-	mpt::object::attribute a = lr["x1"];
-	mpt::line *l = li;
-	
-	int old = l->from.x;
+	// change line item attribute
+	double x1 = li.from.x;
+	mpt::object::attribute a = li["x1"];
 	a = "10";
-	std::cout << old << " -> " << l->from.x << std::endl;
+	std::cout << x1 << " -> " << li.from.x << std::endl;
 	
+	// alternate access types for line item
+	mpt::line &l = li;
+	double x2 = l.to.x;
+	mpt::object &lr = li;
 	lr["x2"] = 4.6;
-	std::cout << lr["x2"] << std::endl;
+	std::cout << x2 << " -> " << l.to.x << std::endl;
+	mpt::color c = l.color;
 	lr["color"] = "#6666";
-	
-	li->unref();
-	ax->unref();
+	std::cout << c << " -> " << l.color << std::endl;
 	
 	return 0;
 }
